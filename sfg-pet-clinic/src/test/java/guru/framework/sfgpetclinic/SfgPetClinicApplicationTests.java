ead179f6ca2007c22a2167ad4c1a4aaa3667dfdb
tyler <tylerjohnson9893@gmail.com>
2020-02-21 18:49:14 -0600

    Initial Projecct Add.

---
+++ sfg-pet-clinic/src/test/java/guru/framework/sfgpetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.framework.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
